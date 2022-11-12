712d7cfdd166d23df3c7f35a940f0d467140fbd6
swaraj-deepzone <swaraj@deepzonetechnologies.com>
2022-11-12 17:41:09 +0530

    initial commit

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,24 @@
 <?php
 
-collect(glob(base_path('/routes/api/*.php')))
-    ->each(function ($path) {
-        require $path;
-    });
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('gambar-profil-upload', [UserController::class, 'gambarProfil']);
+Route::post('dokumen-sokongan', [UserController::class, 'dokumenSokongan']); 
